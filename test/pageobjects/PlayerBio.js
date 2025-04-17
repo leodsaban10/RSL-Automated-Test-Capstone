2e73758ff740c3ec2fd3a32dec10cf715af2ae85
leo.saban <leodsaban@outlook.com>
2025-04-17 08:59:27 -0600

    PlayerBio Testing Completed

--- test/pageobjects/PlayerBio.js
+++ test/pageobjects/PlayerBio.js
@@ -1,27 +1,36 @@
-class LoginPage extends Page {
+import { expect } from '@wdio/globals'; 
 
-    get inputUsername () {
-        return $('#username');
+class Player {
+
+    get RafaelCard () {
+        return $('//*[@id="rafael-cabral-roster"]');
+    }
+    
+    get RafaelCardName () {
+        return $('//*[@aria-label="Learn More About Rafael! "]');
     }
 
-    get inputPassword () {
-        return $('#password');
-    }
-
-    get btnSubmit () {
-        return $('button[type="submit"]');
-    }
-
-
-    async login (username, password) {
-        await this.inputUsername.setValue(username);
-        await this.inputPassword.setValue(password);
-        await this.btnSubmit.click();
+    get RafaelCardHeader () {
+        return $('//h2[contains(text(), "#1 - Rafael Cabral")]');
     }
 
     async RSLHomepage () {
-        return browser.url('https://www.rsl.com/');
+        return browser.url('https://www.rsl.com/club/roster');
     }
+    
+    async RafaelCardExists () {
+        await expect (this.RafaelCard).toExist();
+    }
+
+    async GetRafaelHeaderText () {
+        await expect(this.RafaelCardHeader).toHaveText('#1 - Rafael Cabral');
+    }
+
+    async LearnMoreAboutRafaelBtn () {
+        await this.RafaelCardName.click();
+        await expect(browser).toHaveTitle('Rafael Cabral | Real Salt Lake');
+    }
+
 }
 
-export default new LoginPage();
+export default new Player();
