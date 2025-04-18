2fb6681a919b1ab245421b82bd83242da7c68b4b
leo.saban <leodsaban@outlook.com>
2025-04-18 13:59:02 -0600

    added the test for hover competition list

--- test/specs/AllCompetitionMenuTest.js
+++ test/specs/AllCompetitionMenuTest.js
@@ -4,5 +4,6 @@ describe('All Competition Dropdown Menu', () => {
     it('should open the Schedule Page and click on the All Competitions Dropdown Button', async () => {
         await AllCompetitionsDropdownMenu.RSLSchedulePage()
         await AllCompetitionsDropdownMenu.ClickAllCompetitionsBtn()
+        await AllCompetitionsDropdownMenu.HoverAllCompetitions()
     })
 })
