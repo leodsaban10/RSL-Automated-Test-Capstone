fafe439632b2ca7bd59907e73210167ee6ce581d
leo.saban <leodsaban@outlook.com>
2025-04-18 13:56:32 -0600

    working on hover  all items inside dropdown button

--- test/pageobjects/CompetitionMenu.js
+++ test/pageobjects/CompetitionMenu.js
@@ -1,9 +1,15 @@
-import { expect } from '@wdio/globals'
+import { browser, expect } from '@wdio/globals'
 
 
 class AllCompetitionsDropdownMenu { 
     get AllcompetitionBtn () {
-        return $('//*[@title="Competitions"]');
+        return $('//*[@class="mls-o-buttons__dropdown-button mls-o-buttons__dropdown-button--right "]');
+    }
+    get Competitions () {
+        return $$('//*[@class="mls-o-buttons__dropdown-item"]');
+    }
+    get AllCompetitionList () {
+        return $('//*[@value="all"]');
     }
 
     async RSLSchedulePage () {
@@ -11,7 +17,15 @@ class AllCompetitionsDropdownMenu {
         await expect(browser).toHaveTitle('Schedule | Real Salt Lake');
     }
     async ClickAllCompetitionsBtn () {
-        await this.AllcompetitionBtn.click();;
+        await this.AllcompetitionBtn.click();
+        await expect(this.Competitions).toBeElementsArrayOfSize({ gte: 16 });
+    }
+    //create the hover all competitions function
+    async HoverAllCompetitions () {
+        await this.AllcompetitionBtn.moveTo();
+        await expect(this.Competitions).toBeElementsArrayOfSize({ gte: 16 });
+        await browser.pause(7000);
+        await this.AllcompetitionBtn.click();
     }
 }
 
