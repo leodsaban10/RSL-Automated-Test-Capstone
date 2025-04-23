6c8ea3b8f9f1939c2f052ad07acbf76d995ef23b
leo.saban <leodsaban@outlook.com>
2025-04-23 14:22:30 -0600

    added the assertion for the year and month header

--- test/pageobjects/Calendar.js
+++ test/pageobjects/Calendar.js
@@ -7,19 +7,25 @@ class CalendarUse {
         return $('//*[@aria-label="Date selector"]');
     }
     
-    get ConfirmCalendarYearOpen () {
+    get TodayText () {
         return $('//button[text()="Today"]');
     }
-
-    async Year () {
-        await this.CalendarYear.click();
-        await expect (this.ConfirmCalendarYearOpen).toHaveText('Today');
+    get MonthYearHeader () {
+        return $('//*[@class="mls-o-calendar__title"]');
     }
 
     async RSLSchedulePage () {
         await browser.url('https://www.rsl.com/schedule/');
         await expect(browser).toHaveTitle('Schedule | Real Salt Lake');
     }
+
+    async CalendarOpen () {
+        await this.CalendarYear.click();
+        // await expect (this.TodayText).toHaveText('Today');
+        const monthHeaderText = await this.MonthYearHeader.getText();
+        await expect(monthHeaderText).toContain('April 2025');
+    }
+    
 }
 
 export default new CalendarUse();
--- test/specs/CalendarTest.js
+++ test/specs/CalendarTest.js
@@ -1,9 +1,9 @@
 import CalendarUse from "../pageobjects/Calendar"
 
 describe('Should Open the Calendar', () => {
-    it('should  open the calendar and view the year 2025', async () => {
+    it('should  open the calendar and view the Month and Year 2025', async () => {
         await CalendarUse.RSLSchedulePage()
-        await CalendarUse.Year()
+        await CalendarUse.CalendarOpen()
     })
 })
 
