5860c5cb39440b6b16dade41c08cd434960019b9
leo.saban <leodsaban@outlook.com>
2025-04-30 08:17:13 -0600

    finished the hover over list items

--- test/pageobjects/ThreedotsMenu.js
+++ test/pageobjects/ThreedotsMenu.js
@@ -11,6 +11,12 @@ class ThreedotsMenu {
     get MenuContainer () {
          return $('aside[data-test-id="sidebar-overflow--md"] ul')
     }
+    get News () {
+        return $('(//a[@href="/news/index"])')
+    }
+    // MenuItems (itemName) {
+    //     return $(`//span[@class="mls-o-navigation__item-text"][contains(text(), "${itemName}")]`)
+    // }
 
     async RSLpage () {
         return browser.url('https://www.rsl.com/schedule/');
@@ -20,14 +26,19 @@ class ThreedotsMenu {
         await this.ThreeDotsBtn.click();
         await expect(this.ThreeDotsOpened).toExist();
         await browser.pause(2000);
+        // await this.MenuItems('Youth').click();
+        // await this.News.click();
     }
 
     async HoverOverListItems () {
         await expect(this.MenuContainer).toExist();
         await this.MenuContainer.moveTo();
-            for (let i = 0; i < 6; i++) {
-                await this.MenuContainer.click();
+            for (let i = 0; i < this.MenuContainer.length; i++) {
+                await this.MenuContainer.$('li:nth-child(' + (i + 1) + ')').moveTo();
+                await this.MenuContainer.$('li:last-child').click();
                 await browser.pause(1000);
+                await expect (browser).toHaveTitle('Youth | Real Salt Lake')
+                
             }
     }
 
--- test/specs/Three-DotsMenuTest.js
+++ test/specs/Three-DotsMenuTest.js
@@ -1,7 +1,7 @@
 import ThreedotsMenu from "../pageobjects/ThreedotsMenu"
 
 describe('Three Dots Menu is Clickable', () => {
-    it('should open the Three Dot Menu', async () => {
+    it('should open the Three Dot Menu and Hover over list items', async () => {
         await ThreedotsMenu.RSLpage()
         await ThreedotsMenu.ThreeDotsMenuOpen()
         await ThreedotsMenu.HoverOverListItems()
